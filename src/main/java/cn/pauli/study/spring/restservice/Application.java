da9088e71d26980127801ba0d8b2c0c0ec097f22
Liupo Wu <liupo@hp.com>
2014-01-27 10:36:49 +0800

    replace specific string in files under some folder

--- src/main/java/cn/pauli/study/spring/restservice/Application.java
+++ src/main/java/cn/pauli/study/spring/restservice/Application.java
@@ -18,5 +18,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        /*
+ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        
+         */
     }
 }
